50e03be343554179ca55b69be1e2e4b977a93d1c
rOBI <robertaliaj97@gmail.com>
2023-03-11 03:12:16 +0100

    added Shuffle

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/models/game';
+
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
-  pickCardAnimation = false;
 
+
+export class GameComponent implements OnInit {
+  pickCardAnimation = false;
+  game!: Game; //Das "!" gibt an dass die variable "game" irgendwann später im Code initialisiert wird und TypeScript davon ausgehen sollte, dass sie immer einen Wert hat. Auch "definite assignment assertion". 
+
+
+  ngOnInit() {
+    this.newGame();
+  }
+
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+  
+  
   takeCard() {
     this.pickCardAnimation = true;
   }
---
+++ src/models/game.ts
@@ -0,0 +1,25 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCard: string[] = [];
+    public currentPlayer: number = 0;
+
+    constructor() {
+        for (let i = 1; i < 14; i++) {
+            this.stack.push('spades_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+        this.shuffle();
+    }
+
+    shuffle() {
+        for (let i = this.stack.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            const temp = this.stack[i];
+            this.stack[i] = this.stack[j];
+            this.stack[j] = temp;
+        }
+    }
+}
